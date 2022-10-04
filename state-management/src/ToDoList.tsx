547941b13bbf0990db1be04e3300ee3cee500f56
Chaeeun Lim <limce21@naver.com>
2022-10-04 20:04:29 +0900

    [ADD] 5.8 Form Errors

--- state-management/src/ToDoList.tsx
+++ state-management/src/ToDoList.tsx
@@ -26,12 +26,24 @@ function ToDoList() {
 }
 */
 
+interface IForm {
+	toDo: string;
+	email: string;
+	password: string;
+	password1: string;
+}
+
 function ToDoList() {
-	const { register, watch, handleSubmit, formState } = useForm();
+	const {
+		register,
+		watch,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<IForm>({ defaultValues: { email: '@naver.com' } });
 	const onValid = (data: any) => {
 		console.log(data);
 	};
-	console.log(formState.errors);
+	console.log(errors);
 	return (
 		<div>
 			<form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
@@ -45,6 +57,23 @@ function ToDoList() {
 					})}
 					placeholder='Write to do'
 				/>
+				<input
+					{...register('email', {
+						required: 'Email is required',
+						pattern: { value: /^[A-Za-z0-9._%+-]+@naver\.com/, message: 'Only naver.com emails allowed' }
+					})}
+					placeholder='Write Email'
+				/>
+				<span>{errors?.email?.message}</span>
+				<input {...register('password', { required: 'write here', minLength: 5 })} placeholder='Password' />
+				<span>{errors?.password?.message}</span>
+				<input
+					{...register('password1', {
+						required: 'Password is required'
+					})}
+					placeholder='Password1'
+				/>
+				<span>{errors?.password1?.message}</span>
 				<button>Add</button>
 			</form>
 		</div>
