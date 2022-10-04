0446faaa70eec7ad686013fd5c861f195388e37f
Chaeeun Lim <limce21@naver.com>
2022-10-05 00:23:02 +0900

    [ADD] 9.13 Categories

--- state-management/src/components/ToDo.tsx
+++ state-management/src/components/ToDo.tsx
@@ -1,12 +1,31 @@
-import { IToDo } from '../atom';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from '../atom';
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+	const setToDos = useSetRecoilState(toDoState);
+	const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const {
+			currentTarget: { name }
+		} = event;
+	};
 	return (
 		<li>
 			<span>{text}</span>
-			<button>ToDo</button>
-			<button>Doing</button>
-			<button>Done</button>
+			{category !== 'TO_DO' && (
+				<button name='TO_DO' onClick={onClick}>
+					ToDo
+				</button>
+			)}
+			{category !== 'DOING' && (
+				<button name='DOING' onClick={onClick}>
+					Doing
+				</button>
+			)}
+			{category !== 'DONE' && (
+				<button name='DONE' onClick={onClick}>
+					Done
+				</button>
+			)}
 		</li>
 	);
 }
