d21d7a450a55e609a267a848a04565b84014fce5
Chaeeun Lim <limce21@naver.com>
2022-10-04 23:35:15 +0900

    [ADD] 5.12 Refactoring

---
+++ state-management/src/components/ToDo.tsx
@@ -0,0 +1,14 @@
+import { IToDo } from '../atom';
+
+function ToDo({ text }: IToDo) {
+	return (
+		<li>
+			<span>{text}</span>
+			<button>ToDo</button>
+			<button>Doing</button>
+			<button>Done</button>
+		</li>
+	);
+}
+
+export default ToDo;
