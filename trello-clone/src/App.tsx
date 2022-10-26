9f6c45b215f0aef2b34b2b10cc8c00ab2e8c2989
Chaeeun Lim <limce21@naver.com>
2022-10-26 16:42:47 +0900

    [7.6] Reordering part Two

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -34,7 +34,17 @@ background-color : ${(props) => props.theme.cardColor}}`;
 
 function App() {
 	const [toDos, setToDos] = useRecoilState(toDoState);
-	const onDragEnd = ({ destination, source }: DropResult) => {};
+	const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+		if (!destination) return;
+		setToDos((oldToDos) => {
+			const copyToDos = [...oldToDos];
+			// 1) Delete item on source.index
+			copyToDos.splice(source.index, 1);
+			// 2) Put back the item on the destination.index
+			copyToDos.splice(destination?.index, 0, draggableId);
+			return copyToDos;
+		});
+	};
 	return (
 		<DragDropContext onDragEnd={onDragEnd}>
 			<Wrapper>
@@ -43,7 +53,7 @@ function App() {
 						{(magic) => (
 							<Board ref={magic.innerRef} {...magic.droppableProps}>
 								{toDos.map((toDo, index) => (
-									<Draggable draggableId={toDo} index={index}>
+									<Draggable key={toDo} draggableId={toDo} index={index}>
 										{(magic) => (
 											<Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
 												{toDo}
