e05f5f9727f73929c155a6990882cf50b1e5d067
Chaeeun Lim <limce21@naver.com>
2022-10-26 14:54:55 +0900

    [7.1] Set Selectors

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -3,14 +3,17 @@ import { hourSelector, minuteState } from './atom';
 
 function App() {
 	const [minutes, setMinutes] = useRecoilState(minuteState);
-	const hours = useRecoilValue(hourSelector);
+	const [hours, setHours] = useRecoilState(hourSelector);
 	const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
 		setMinutes(parseInt(event.currentTarget.value));
+	};
+	const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+		setHours(parseInt(event.currentTarget.value));
 	};
 	return (
 		<div>
 			<input value={minutes} onChange={onMinutesChange} type='number' placeholder='Minutes' />
-			<input value={hours} type='number' placeholder='Hours' />
+			<input value={hours} onChange={onHoursChange} type='number' placeholder='Hours' />
 		</div>
 	);
 }
