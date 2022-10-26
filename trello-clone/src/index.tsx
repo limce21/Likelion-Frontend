73fa10718a58fd479647fc6bf888a02f4529b70a
Chaeeun Lim <limce21@naver.com>
2022-10-26 16:10:01 +0900

    [7.4] Styles and Placeholders

--- trello-clone/src/index.tsx
+++ trello-clone/src/index.tsx
@@ -8,6 +8,10 @@ import { darkTheme } from './theme';
 const GlobalStyle = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 
+	*[hidden] {
+		display: none;
+	}
+
 	html, body, div, span, applet, object, iframe,
 	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 	a, abbr, acronym, address, big, cite, code,
@@ -21,22 +25,14 @@ const GlobalStyle = createGlobalStyle`
 	figure, figcaption, footer, header, hgroup,
 	main, menu, nav, output, ruby, section, summary,
 	time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
 	}
-	/* HTML5 display-role reset for older browsers */
-	article, aside, details, figcaption, figure,
-	footer, header, hgroup, main, menu, nav, section {
-	display: block;
-	}
-	/* HTML5 hidden-attribute fix for newer browsers */
-	*[hidden] {
-		display: none;
-	}
+
 	body {
 	line-height: 1;
 	}
@@ -63,7 +59,7 @@ const GlobalStyle = createGlobalStyle`
 	body{
 		font-family: 'Source Sans Pro', sans-serif;
 		background-color: ${(props) => props.theme.bgColor};
-		color: ${(props) => props.theme.textColor}
+		color: black;
 	}
 
 	a{
