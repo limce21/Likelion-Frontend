312fe81ae610cd85f1fe0e3cd8f9e5a88a9f6564
Chaeeun Lim <limce21@naver.com>
2022-10-27 11:35:38 +0900

    [7.12] Final Styles

--- trello-clone/src/Components/DraggableCard.tsx
+++ trello-clone/src/Components/DraggableCard.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
-const Card = styled.div`
-border-radius: 5px;
-padding: 10px 10px;
-margin-bottom: 5px;
-background-color : ${(props) => props.theme.cardColor}}`;
+const Card = styled.div<{ isDragging: boolean }>`
+	border-radius: 5px;
+	padding: 10px 10px;
+	margin-bottom: 5px;
+	background-color: ${(props) => (props.isDragging ? '#74b9ff' : props.theme.cardColor)};
+	box-shadow: ${(props) => (props.isDragging ? '0px 2px 5px rgba(0,0,0,0.5)' : 'none')};
+`;
 
 interface IDraggableCardProps {
 	toDo: string;
@@ -16,8 +18,8 @@ interface IDraggableCardProps {
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
 	return (
 		<Draggable key={toDo} draggableId={toDo} index={index}>
-			{(magic) => (
-				<Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+			{(magic, snapshot) => (
+				<Card isDragging={snapshot.isDragging} ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
 					{toDo}
 				</Card>
 			)}
