4fd214b2b8987960799b3deea6425a65df3f1059
Chaeeun Lim <limce21@naver.com>
2022-11-01 12:59:55 +0900

    [1.2] Actions

--- Learning Vanila-Redux and React-Redux/vanila-redux/src/index.js
+++ Learning Vanila-Redux and React-Redux/vanila-redux/src/index.js
@@ -4,9 +4,22 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const counterModifier = (count = 0) => {
-	count++;
-	count--;
-	return count;
+const counterModifier = (count = 0, action) => {
+	if (action.type === 'ADD') {
+		return count + 1;
+	} else if (action.type === 'MINUS') {
+		return count - 1;
+	} else {
+		return count;
+	}
 };
 const countStore = createStore(counterModifier);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+
+console.log(countStore.getState());
