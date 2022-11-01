20186cdf457c0d53add9060b73856f0b3458dd49
Chaeeun Lim <limce21@naver.com>
2022-11-01 13:14:23 +0900

    [1.4] Recap Refactor

--- Learning Vanila-Redux and React-Redux/vanila-redux/src/index.js
+++ Learning Vanila-Redux and React-Redux/vanila-redux/src/index.js
@@ -4,13 +4,17 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const counterModifier = (count = 0, action) => {
-	if (action.type === 'ADD') {
-		return count + 1;
-	} else if (action.type === 'MINUS') {
-		return count - 1;
-	} else {
-		return count;
+	switch (action.type) {
+		case ADD:
+			return count + 1;
+		case MINUS:
+			return count - 1;
+		default:
+			return count;
 	}
 };
 const countStore = createStore(counterModifier);
@@ -20,5 +24,5 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 
-add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
-minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
+add.addEventListener('click', () => countStore.dispatch({ type: ADD }));
+minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
