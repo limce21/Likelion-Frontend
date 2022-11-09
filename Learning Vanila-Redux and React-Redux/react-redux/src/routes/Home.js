e2c2a53e62592c6676a9ddc917112e303a5c8970
Chaeeun Lim <limce21@naver.com>
2022-11-09 15:17:30 +0900

    [3.3] mapDispatchToProps

--- Learning Vanila-Redux and React-Redux/react-redux/src/routes/Home.js
+++ Learning Vanila-Redux and React-Redux/react-redux/src/routes/Home.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { actionCreators } from '../store';
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
 	const [text, setText] = useState('');
 	function onChange(e) {
 		setText(e.target.value);
 	}
 	function onSubmit(e) {
 		e.preventDefault();
-		console.log(text);
+		addToDo(text);
+		setText('');
 	}
 	return (
 		<>
@@ -21,8 +23,17 @@ function Home({ toDos }) {
 		</>
 	);
 }
+
 function mapStateToProps(state) {
 	return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+	return {
+		addToDo: (text) => {
+			dispatch(actionCreators.addToDo(text));
+		}
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
